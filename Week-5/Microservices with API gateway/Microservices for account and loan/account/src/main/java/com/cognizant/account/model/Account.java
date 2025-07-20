2a2bc212f0a392242c23b02cc397874b3827c800
ManaswiBatchu <149962917+ManaswiBatchu@users.noreply.github.com>
2025-07-20 20:09:40 +0530

    Delete Week-5/Microservices with API gateway/Microservices for account and loan directory

--- Week-5/Microservices with API gateway/Microservices for account and loan/account/src/main/java/com/cognizant/account/model/Account.java
+++
@@ -1,5 +0,0 @@
-package com.cognizant.account.model;
-
-public class Account {
-
-}
--- Week-5/Microservices with API gateway/Microservices for account and loan/loan/src/main/java/com/cognizant/loan/Loan.java
+++
@@ -1,4 +0,0 @@
-package com.cognizant.loan;
-
-public record Loan(String number, String type, long loan, long emi, int tenure) {
-}
